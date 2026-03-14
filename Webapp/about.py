f423dbd0f31d0e0b1b049ed80e7ed207ef4b38ad
Devvrath <87268413+devvrath123@users.noreply.github.com>
2026-03-14 17:11:55 +0100

    webapp update2

--- Webapp/about.py
+++ Webapp/about.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("👤 Contact Info")
+st.title("👤 About this project")
 st.divider()
-st.write("This web-app was created by Devvrath S. Find contact links below:")
+st.write("This web-app was created as a hobby project. Find my contact links below:")
 with st.container(horizontal=True):
     st.link_button(label="GitHub", url="https://github.com/devvrath123")
     st.link_button(label="LinkedIn", url="https://www.linkedin.com/in/devvrath-s-18b77b257/")
